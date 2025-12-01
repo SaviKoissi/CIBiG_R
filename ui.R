8dd8ac07056c2509b38b112e96a2afb4324d04a3
Koissi <70168615+SaviKoissi@users.noreply.github.com>
2025-12-01 12:49:47 +0000

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,19 +1,16 @@
 library(shiny)
+library(shinyjs)
 
-shinyUI(fluidPage(
-  
-  titlePanel("Bioinformatics Live Quiz"),
-  
+fluidPage(
+  useShinyjs(),         # for hide/show
+  textOutput("timer"),
   textInput("student", "Enter your full name:"),
   
-  h3("Multiple-choice questions"),
   uiOutput("mcq_ui"),
-  
-  h3("Code questions (write R code)"),
   uiOutput("code_ui"),
   
-  actionButton("submit", "Submit Answers", class = "btn-primary"),
-  br(), br(),
+  actionButton("submit", "Submit Answers"),
+  actionButton("quit", "Quit", style = "display:none;"),
+  br(),
   textOutput("result")
-))
-
+)
